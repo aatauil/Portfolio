ddf4127429c95e773f7a6f6252f6931ef7a6a0d1
aatauil <achrafatauil@hotmail.com>
2020-05-12 13:50:37 +0200

    changed scss files + add nav
    -Added normalize + poppins Google font
    -Added variables in _variables.scss
    -added icon triangle

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,21 @@
 <template>
   <div id="app">
+    <router-view>
+
+    </router-view>
+    <button class="btn-menu btn-menu_open">MENU</button>
+    <MyNav/>
   </div>
 </template>
 
 <script>
 
+import MyNav from "./components/MyNav";
+
 export default {
   name: 'App',
   components: {
-
+    MyNav
   }
 }
 </script>
@@ -23,11 +30,27 @@ html{
 }
 
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: $poppins;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+
+}
+
+.btn-menu{
+  position: absolute;
+  right: 1rem;
+  bottom: 1rem;
+}
+
+// Global classes
+
+.active-link{
+    opacity: 1;
+    &::before{
+        content: url("./assets/icons/menu-triangle.svg");
+        margin-right: 1rem;
+    }
 }
 </style>
---
+++ src/components/MyNav.vue
@@ -0,0 +1,51 @@
+<template>
+  <nav class="nav-container">
+      <div class="nav-list">
+          <router-link class="nav-item" to="/">Home</router-link>
+          <router-link class="nav-item" to="/MySkills">My skills</router-link>
+          <router-link class="nav-item" to="/MyWork">My work</router-link>
+      </div>
+      <button class="btn-menu btn-menu_open">Close</button>
+  </nav>
+</template>
+
+<script>
+export default {
+    name: "MyNav"
+
+}
+</script>
+
+<style lang="scss">
+
+@import "../scss/main.scss";
+
+.nav-container{
+    position: fixed;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    width: 100vw;
+    height: 100vh;
+    background-color: $color-black;
+    top: 0;
+    left: 0;
+    font-weight: bold;
+}
+
+.nav-list{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+    height: 50%;
+    padding: 0 3rem;
+}
+
+.nav-item{
+    font-size: $size-menu;
+    color: white;
+    text-decoration: none;
+}
+
+
+</style>
