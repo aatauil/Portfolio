d9821862ae95151878b79b648cb86a1b2994feae
aatauil <achrafatauil@hotmail.com>
2020-05-13 15:14:22 +0200

    Added Logo & ui/ux to design skills

--- src/components/skills/Design.vue
+++ src/components/skills/Design.vue
@@ -1,11 +1,27 @@
 <template>
-  <section>Design</section>
+   <section class="focused-skill_container">
+      <SingleSkill
+          title= "UI/UX"
+          text= "My first and most prefered programming language.  I can say with confident that I have a very good understanding of Javascript. I know how the language gets compiled in the browser and therefor bugs usually get resolve very quickly.<br><br> I use ES6 syntax and am currently learning ES7 & ES8 and ofcoarse keeping on eye out for es.next. Very excited!<br><br> You never finish learning Javascript there is always more!"
+          v-bind:tags="['webpack', 'vuejs', 'Jquery', 'I18next']"
+          imageName="uiux.png"
+      />
+       <SingleSkill
+          title= "logo"
+          text= "My first and most prefered programming language.  I can say with confident that I have a very good understanding of Javascript. I know how the language gets compiled in the browser and therefor bugs usually get resolve very quickly.<br><br> I use ES6 syntax and am currently learning ES7 & ES8 and ofcoarse keeping on eye out for es.next. Very excited!<br><br> You never finish learning Javascript there is always more!"
+          v-bind:tags="['webpack', 'vuejs', 'Jquery', 'I18next']"
+          imageName="uiux.png"
+      />
+   </section>
 </template>
 
 <script>
+import SingleSkill from "../SingleSkill";
 export default {
-    name: "Design"
-
+    name: "Design",
+    components: {
+        SingleSkill
+    }
 }
 </script>
 
